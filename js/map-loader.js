38e07a5c206127d304bace3f99cd92ea4bcb166a
sharathdt <sharath.dt@gmail.com>
2022-05-24 10:11:09 +0530

    update site

--- js/map-loader.js
+++ js/map-loader.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-        $('iframe#map').attr('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3880.900653868912!2d75.2415365145659!3d13.418476090563857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbb43d719008ba9%3A0x23a0aef5a7eb0811!2sSuvidha+Food+and+Bevarages!5e0!3m2!1sen!2sin!4v1498561376155');    
+        $('iframe#map').attr('src', 'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15523.598646030441!2d75.243725!3d13.4185375!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x98f237bdef89c779!2z4LKr4LON4LKw4LOC4LKf4LONIOCyn-CzjeCysOCzgOCyn-CzjSDgsprgsr_gsqrgs43gsrjgs418IOCyleCyvuCysOCyguCypOCzjeCyuOCzjSDgsqvgs4HgsqHgs43gsrjgs40g4LKq4LK_4LK14LK_4LKf4LK_LiDgso7gsrLgs43igIzgsp_gsr_gsqHgsr8gfCDgsqzgs4bgsoLgspfgsrPgs4LgsrDgs4EgfCDgsprgsr_gsqrgs43gsrjgs40g4LKG4LKo4LON4LKy4LOI4LKo4LON!5e0!3m2!1skn!2sin!4v1653366770469!5m2!1skn!2sin');    
         });
